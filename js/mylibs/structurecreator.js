212b74ee8198afa10c3dec68723e2227b88ee20f
Ed Moore <eddie.moore@gmail.com>
2012-01-14 02:49:04 +0800

    Added jszip to generate the final zip file

--- js/mylibs/structurecreator.js
+++ js/mylibs/structurecreator.js
@@ -1,7 +1,58 @@
 (function(window) {
 	
 	var StructureCreator = {
+		zip : null,
+		schema_file : null,
+		total_folders : 0,
+		folders_checked : 0,
+		total_files : 0,
+		files_checked : 0,
+
+		//Start The creation of the files
+		start : function() {
+			StructureCreator.zip = new JSZip();
+			StructureCreator.total_folders = 0;
+
+			if (StructureCreator.schema_file.type === "text/xml") {
+				var reader = new FileReader();
+				reader.onload = (function(file){
+					return function(e) {
+						var xml = StructureCreator.xml.str2xml(e.target.result);
+						StructureCreator.total_folders = xml.getElementsByTagName('folder').length;
+						StructureCreator.total_files = xml.getElementsByTagName('file').length;
+						StructureCreator.folderLoop(xml, StructureCreator.zip);
+					};
+				})(StructureCreator.schema_file);
+				reader.readAsText(StructureCreator.schema_file);
+			}
+		},
 		
+		folderLoop : function (xml, folder) {
+			var node, i, file, new_folder;
+			for (i = xml.childNodes.length - 1; i >= 0; i--) {
+				node = xml.childNodes[i];
+				if (node.nodeName === "folder") {
+					new_folder = folder.folder(node.getAttribute('name'));
+					//create folder in zip.
+					StructureCreator.folderLoop(node, new_folder);
+				} else if (node.nodeName === "file") {
+					log(node.textContent);
+
+					folder.add(node.getAttribute('name'), "Hello World\n");
+				}
+			};
+			StructureCreator.folders_checked += 1;
+			if (StructureCreator.folders_checked === StructureCreator.total_folders) {
+				log(StructureCreator.zip);
+				//StructureCreator.saveZip();
+			}
+		},
+
+		//Saves all the files as a zip file
+		saveZip : function() {
+			content = StructureCreator.zip.generate();
+			location.href="data:application/zip;base64,"+content;
+		}
 	}
 
 	window.StructureCreator = window.SC = StructureCreator;
--- js/script.js
+++ js/script.js
@@ -2,64 +2,12 @@
 
 */
 
-var schema_file;
-
-
 function schemaFileSelect(evt) {
-	schema_file = evt.target.files[0]; // File
-	log(schema_file);
+	StructureCreator.schema_file = evt.target.files[0]; // File
+	log(StructureCreator.schema_file);
 }
 
-function startCreating() {
-
-	//Read Schema file
-	if (schema_file.type === "text/xml") {
-		var reader = new FileReader();
-		reader.onload = (function(file){
-			return function(e) {
-	          //var span = document.createElement('span');
-	          //log(e.target);
-	          //log(e.target.result);
-	          var xml = StructureCreator.xml.str2xml(e.target.result);
-	          folderLoop(xml);
-	          //document.getElementById('list').insertBefore(span, null);
-	        };
-		})(schema_file);
-		reader.readAsText(schema_file);
-	}
-}
-
-function folderLoop(xml) {
-	log(xml);
-	var node;
-	for (var i = xml.childNodes.length - 1; i >= 0; i--) {
-		var node = xml.childNodes[i];
-		if (node.nodeName === "folder") {
-			log("it's a folder");
-			//create folder in zip.
-			folderLoop(node);
-		} else if (node.nodeName === "file") {
-			log("it's a file");
-			log(node.getAttribute('name'));
-		}
-	};
-}
 
 document.getElementById('schema_file').addEventListener('change', schemaFileSelect, false);
-document.getElementById('create_project').addEventListener('click', startCreating, false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById('create_project').addEventListener('click', StructureCreator.start, false);
 
