241bedcb6d3f524e1ba1d94e2f4498ae24713f6f
Ed Moore <eddie.moore@gmail.com>
2012-02-07 23:59:32 -0800

    changed structure of code and how it gets initialised

--- js/mylibs/structurecreator.js
+++ js/mylibs/structurecreator.js
@@ -1,124 +1,184 @@
-(function(window) {
+(function (window) {
+
+	//private vars
+	var _total_folders   = 0,
+		_total_files     = 0,
+		_folders_created = 0,
+		_files_created   = 0;
+
+	//public vars
 	
-	var StructureCreator = {
-		zip : null,
-		schema_file : null,
-		total_folders : 0,
-		folders_checked : 0,
-		total_files : 0,
-		files_checked : 0,
+	
+	var StructureCreator = function () {
+		//this.schema_file = null;
+	};
 
-		//Start The creation of the files
-		start : function() {
-			StructureCreator.zip             = new JSZip();
-			StructureCreator.total_folders   = 0;
-			StructureCreator.total_files     = 0;
-			StructureCreator.folders_checked = 0;
-			StructureCreator.files_checked   = 0;
+	//StructureCreator.prototype = {};
+	StructureCreator.schema_file = null;
+	StructureCreator.zip = null;
 
-			if (StructureCreator.schema_file.type === "text/xml") {
+	StructureCreator.prototype.init = function() {
+		_total_files     = 0;
+		_total_folders   = 0;
+		_folders_created = 0;
+		_files_created   = 0;
+
+		this.loadSchemaFile();
+	};
+
+	StructureCreator.prototype.loadSchemaFile = function () {
+		log(this);
+		if (StructureCreator.schema_file !== null) {
+			if (typeof StructureCreator.schema_file === "string") {
+				var ext = StructureCreator.schema_file.substr(StructureCreator.schema_file.lastIndexOf('.') + 1);
+				ext = ext.toLowerCase();
+
+				if (typeof this.schemas[ext] !== "undefined") {
+					log('load the file');
+					this.net.URLLoader(this.schema_file, "GET", null, this.schemas[ext].init);
+				} else {
+					console.log('There is nothing to open a ' + ext + ' file with. :(');
+				}
+
+			} else {
+				//TODO load schema using File Reference
+				console.log('schema is a file on the computer so use FileReader');
+				if (StructureCreator.schema_file.type === "text/xml") {
+					log(this.schemas);
+					this.schemas.xml.readXMLFromFile();
+				} else {
+					console.error("Cannot read schema file");
+				}
+			}
+		} else {
+			//TODO dispatch event that schema is not created.
+			console.log('no schema file to load');
+		}
+	};
+
+	StructureCreator.prototype.isCompleted = function() {
+		if (_folders_created === _total_folders && _files_created === _total_files) {
+			return true;
+		}
+		return false;
+	};
+	
+
+
+
+
+	//Schema types
+	StructureCreator.prototype.schemas = {
+		//XML
+		xml : {
+			init : function (response) {
+				log('load the xml');
+				log(response);
+				var xml = StructureCreator.schemas.xml.str2xml(result);
+				_total_folders = xml.getElementsByTagName('folder').length;
+				_total_files   = xml.getElementsByTagName('file').length;
+				StructureCreator.schemas.xml.folderLoop(xml.childNodes[0], null);
+			},
+
+			readXMLFromFile : function () {
+				log('read xml from file');
 				var reader = new FileReader();
 				reader.onload = (function(file){
-					return function(e) {
-						var xml = StructureCreator.xml.str2xml(e.target.result);
-						StructureCreator.total_folders = xml.getElementsByTagName('folder').length;
-						StructureCreator.total_files   = xml.getElementsByTagName('file').length;
-						StructureCreator.folderLoop(xml, StructureCreator.zip);
-					};
+					log('file loaded');
+					//return function(e) {
+						/*var xml = StructureCreator.schemas.xml.str2xml(e.target.result);
+						_total_folders = xml.getElementsByTagName('folder').length;
+						_total_files   = xml.getElementsByTagName('file').length;
+						StructureCreator.schemas.xml.folderLoop(xml.childNodes[0], null);*/
+					//StructureCreator.schemas.xml.init()
+					log(file.target.result);
+					StructureCreator.schemas.xml.init(e.target.result);
+						//StructureCreator.folderLoop(xml, StructureCreator.zip);
+					//};
 				})(StructureCreator.schema_file);
+				log(StructureCreator.schema_file);
 				reader.readAsText(StructureCreator.schema_file);
-			}
-		},
-		
-		folderLoop : function (xml, folder) {
-			var node, i, file, new_folder;
-			for (i = xml.childNodes.length - 1; i >= 0; i--) {
+			},
 
-				node = xml.childNodes[i];
-				if (node.nodeName === "folder") {
+			folderLoop : function (xml, folder) {
+				var node, i, file, new_folder;
+				for (i = xml.childNodes.length - 1; i >= 0; i--) {
 
-					new_folder = folder.folder(node.getAttribute('name'));
-					//create folder in zip.
-					StructureCreator.folderLoop(node, new_folder);
-
-				} else if (node.nodeName === "file") {
-					
-					if (node.firstChild) {
-						log(node.firstChild.data);
-						folder.add(node.getAttribute('name'), node.firstChild.data);
-						StructureCreator.files_checked += 1;
-					} else {
-						//TODO Get data from url attribute
-						var f = StructureCreator.fileLoader(folder, node);
+					node = xml.childNodes[i];
+					if (node.nodeName === "folder") {
+						log('create folder : ' + node.getAttribute('name'));
+						new_folder = null;//folder.folder(node.getAttribute('name'));
+						//create folder in zip.
+						StructureCreator.schemas.xml.folderLoop(node, new_folder);
 						
-						//folder.add(node.getAttribute('name'), "Hello World\n");
+					} else if (node.nodeName === "file") {
+						
+						if (node.firstChild) {
+							log('create file : ' + node.getAttribute('name'));
+							log(node.firstChild.data);
+							//folder.add(node.getAttribute('name'), node.firstChild.data);
+							_files_created += 1;
+						} else {
+							//TODO Get data from url attribute
+							//var f = StructureCreator.fileLoader(folder, node);
+							
+							//folder.add(node.getAttribute('name'), "Hello World\n");
+						}
+
 					}
-
-				}
-			};
-			StructureCreator.folders_checked += 1;
-			//Check if complete
-			if (StructureCreator.isCompleted()) {
-				log(StructureCreator.zip);
-				StructureCreator.saveZip();
-			}
-		},
-
-		fileLoader : function (folder, node) {
-			/*var client = new XMLHttpRequest();
-			client.open('GET', node.getAttribute('url'));
-			client.onreadystatechange = function() {
-				//alert(client.responseText);
-				folder.add(node.getAttribute('name'), client.responseText);
-				StructureCreator.files_checked += 1;
+				};
+				_folders_created += 1;
 				//Check if complete
 				if (StructureCreator.isCompleted()) {
-					log(StructureCreator.zip);
-					StructureCreator.saveZip();
+					log('all files and folders created');
+					//log(StructureCreator.zip);
+					//StructureCreator.saveZip();
 				}
-			}
-			client.send();*/
+			},
 
-			var xhr = new XMLHttpRequest();
-			xhr.open("GET", node.getAttribute('url'), true);
+			str2xml : function (str) {
+				var doc;
+				if (window.ActiveXObject) { 
+					doc = new ActiveXObject("Microsoft.XMLDOM"); 
+					doc.async = "false"; 
+					doc.loadXML(strXML); 
+				} else { 
+					// code for Mozilla, Firefox, Opera, etc. 
+					var parser = new DOMParser();
+					doc = parser.parseFromString(strXML,"text/xml"); 
+				}// documentElement always represents the root node 
+				return doc;
+			}
+		}
+	};
+
+
+
+
+	//StructureCreator.net functions
+	StructureCreator.prototype.net = function() {
+		this.URLLoader = function (url, method, params, callback) {
+			var xhr = new XMLHttpRequest(),
+				method = method || "GET",
+				params = params || null,
+				callback = callback || function (response) {};
+
 			xhr.onreadystatechange = function(){
 			  if ( xhr.readyState == 4 ) {
 			    if ( xhr.status == 200 ) {
-			      //document.body.innerHTML = "My Name is: " + xhr.responseText;
-			      log(xhr.responseText);
+					console.log(xhr.responseText);
+					callback(xhr.responseText);
 			    } else {
-			      //document.body.innerHTML = "ERROR";
-			      log("Error loading from " + node.getAttribute('url'));
+			    	console.log("Error loading from " + url);
 			    }
 			  }
 			};
-			xhr.send(null);
-
-			/*$.get(node.getAttribute('url'), function(response, status, xhr) {
-				log(response);
-			});*/
-
-			/*var o = document.createElement('osloader');
-			$(o).load(node.getAttribute('url'), function (response, status, xhr){
-				log(response);
-			});*/
-		},
-
-		isCompleted : function () {
-			if ((StructureCreator.folders_checked === StructureCreator.total_folders)
-					&& (StructureCreator.files_checked === StructureCreator.total_files)) {
-				return true;
-			} else {
-				return false;
-			}
-		},
-
-		//Saves all the files as a zip file
-		saveZip : function() {
-			content = StructureCreator.zip.generate();
-			location.href="data:application/zip;base64,"+content;
+			xhr.open(method, url, true);
+			xhr.send(params);
 		}
-	}
+	};
 
-	window.StructureCreator = window.SC = StructureCreator;
-})(window);
+	//Expose StructureCreator to the global object
+	window.StructureCreator = StructureCreator;
+
+}(window));
--- js/script.js
+++ js/script.js
@@ -2,11 +2,35 @@
 
 */
 
-function schemaFileSelect(evt) {
+/*function schemaFileSelect(evt) {
 	StructureCreator.schema_file = evt.target.files[0]; // File
 	log(StructureCreator.schema_file);
 }
 
-
+$(function() {
+	$('#create_project').click(function() {
+		StructureCreator.start();
+	});
+});
 document.getElementById('schema_file').addEventListener('change', schemaFileSelect, false);
-document.getElementById('create_project').addEventListener('click', StructureCreator.start, false);
+//document.getElementById('create_project').addEventListener('click', StructureCreator.start, false);
+*/
+window.domain = 'github.com';
+
+var sc = new StructureCreator(),
+	schema_file = document.getElementById('schema_file'),
+	create_btn = document.getElementById('create_project');
+
+console.log(StructureCreator.schema_file);
+function schemaFileSelect(evt) {
+	StructureCreator.schema_file = evt.target.files[0]; // File
+	log('schema file');
+	log(StructureCreator.schema_file);
+}
+
+function startCreation() {
+	sc.init();
+}
+
+schema_file.addEventListener('change', schemaFileSelect);
+create_btn.addEventListener('click', startCreation);
